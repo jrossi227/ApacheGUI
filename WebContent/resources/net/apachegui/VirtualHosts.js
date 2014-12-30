1f6c8fe3fb757f8b155febb165d7d42004731ee1
jrossi227 <jonathan.m.rossi@gmail.com>
2014-12-30 13:28:35 -0500

    Continue work on virtual hosts

--- WebContent/resources/net/apachegui/VirtualHosts.js
+++ WebContent/resources/net/apachegui/VirtualHosts.js
@@ -6,13 +6,18 @@ define([ "dojo/_base/declare",
          "dojo/data/ItemFileWriteStore",
          "dojox/grid/DataGrid",
          "net/apachegui/TitlePane",
-         "dijit/Tree",
-         "dijit/tree/ForestStoreModel"
-], function(declare, dom, request, registry, on, ItemFileWriteStore, DataGrid, TitlePane, Tree, ForestStoreModel){
+         "net/apachegui/RefreshableTree",
+         "dijit/tree/ForestStoreModel",
+         "dojo/store/Observable",
+         "dijit/Menu",
+         "dijit/MenuItem"
+], function(declare, dom, request, registry, on, ItemFileWriteStore, DataGrid, TitlePane, RefreshableTree, ForestStoreModel, Observable, Menu, MenuItem){
     
     declare("net.apachegui.VirtualHosts", null, {
         
         currentHierarchicalHostSummaryCount: 0,
+        currentTreeSummaryCount: 0,
+        currentTreeId: '',
         initialized: false,
         
         init : function() {
@@ -23,7 +28,119 @@ define([ "dojo/_base/declare",
             }
         },
         
+        //------------------TREE VIRTUAL HOSTS-------------------------//
+        
+        updateAllTreeProperties: function(hosts) {
+            
+            //compare network info
+            
+            //reset line of start
+            
+            //reset line of end
+            
+            //reset network info
+            
+        },
+        
+        reloadTree: function(tree) {
+            //request all virtual hosts
+            //tree.model.store= new ItemFileWriteStore({});
+            
+            tree.reload();
+            this.updateAllTreeProperties();
+        },
+        
         populateTreeVirtualHosts: function() {
+           var that = this;
+            
+           var buildTreeHost = function(host,globalServerName) {
+               //build the tree
+               var id = "tree-" + that.currentTreeSummaryCount;
+               
+               var storeId = 'store-' + id;
+               
+               var storeData = host.tree;
+               var store = new ItemFileWriteStore({
+                   data: storeData,
+                   id: storeId
+               });
+               store = new Observable(store);
+               
+               var modelId = 'model-' + id;
+               
+               var treeModel = new ForestStoreModel({
+                   store: store,
+                   query: {"type": "VirtualHost"},
+                   rootId: 0,
+                   childrenAttrs: ["children"],
+                   id: modelId
+               });
+
+               var treeId = 'tree-' + id;
+               
+               var myTree = new RefreshableTree({
+                   model: treeModel,
+                   showRoot: false,
+                   autoExpand: true,
+                   id: treeId
+               });
+               myTree.set({
+                   lineOfStart: host.lineOfStart,
+                   lineOfEnd: host.lineOfEnd,
+                   networkInfo: host.NetworkInfo
+               });
+               
+               var serverName = (host.ServerName == '' ? (globalServerName == '' ? 'unknown' : globalServerName)  : host.ServerName);
+               
+               var networkInfoValue = '';
+               var networkInfo = host.NetworkInfo;
+               for(var j=0; j<networkInfo.length; j++) {
+                   networkInfoValue += networkInfo[j].value;
+               }
+               
+               var div = document.createElement('div');
+               div.id = id;
+               div.innerHTML = '<h4>' + serverName + ' ' + networkInfoValue + '</h4>';
+               div.appendChild(myTree.domNode);
+               
+               dom.byId('tree_virtual_host_container').appendChild(div);
+               
+               myTree.startup();
+               
+               var menu = new Menu({
+                   targetNodeIds: [id],
+                   selector: ".dijitTreeNode"
+               });
+               
+               menu.addChild(new MenuItem({
+                   label: "Delete",
+                   onClick: function() {
+                       console.log('i was clicked');
+                       that.reloadTree(myTree);
+                   }
+               }));
+               
+               menu.addChild(new MenuItem({
+                   label: "Edit",
+                   onClick: function() {
+                       console.log('i was clicked');
+                   }
+               }));
+               
+               on(menu, "focus", function(e) {
+                   var tn = registry.getEnclosingWidget(this.currentTarget);
+                   console.log(tn.item.id);
+                   console.log(tn.item.type);
+                   console.log(tn.item.value);
+                   that.currentTreeId = treeId;
+                   console.log(that.currentTreeId);
+               });
+                              
+               // when we right-click anywhere on the tree, make sure we open the menu
+               //menu.bindDomNode(myTree.domNode);
+               
+               that.currentTreeSummaryCount++;
+           };
            
            var thisdialog = net.apachegui.Util.noCloseDialog('Loading', 'Loading Tree Hosts...');
            thisdialog.show();
@@ -42,42 +159,8 @@ define([ "dojo/_base/declare",
                    var hosts = data.hosts;
                    var globalServerName = data.ServerName;
                    
-                   var host;
-                   for(var i=0; i<hosts.length; i++) {
-                       host = hosts[i];
-                       
-                     //build the tree
-                       var storeData = host.tree;
-                       var store = new ItemFileWriteStore({data: storeData});
-                       
-                       var treeModel = new ForestStoreModel({
-                           store: store,
-                           query: {"type": "VirtualHost"},
-                           rootId: 0,
-                           childrenAttrs: ["children"]
-                       });
-
-                       var myTree = new Tree({
-                           model: treeModel,
-                           showRoot: false,
-                           autoExpand: true
-                       });
-                       
-                       var serverName = (host.ServerName == '' ? (globalServerName == '' ? 'unknown' : globalServerName)  : host.ServerName);
-                       
-                       var networkInfoValue = '';
-                       var networkInfo = host.NetworkInfo;
-                       for(var j=0; j<networkInfo.length; j++) {
-                           networkInfoValue += networkInfo[j].value;
-                       }
-                       
-                       var div = document.createElement('div');
-                       div.innerHTML = '<h4>' + serverName + ' ' + networkInfoValue + '</h4>';
-                       div.appendChild(myTree.domNode);
-                       
-                       dom.byId('tree_virtual_host_container').appendChild(div);
-                       
-                       myTree.startup();
+                   for(var i=0; i<hosts.length; i++) {                       
+                       buildTreeHost(hosts[i],globalServerName);
                    }
                                        
                    thisdialog.remove();
@@ -89,10 +172,14 @@ define([ "dojo/_base/declare",
            );
         },
         
+        //------------------END OF TREE VIRTUAL HOSTS-------------------------//
+        
+        //------------------HIERARCHICAL VIRTUAL HOSTS-------------------------//
+        
         populateHierarchicalVirtualHosts : function() {
             var that = this;
             
-            var buildVirtualHost = function(name, vhost, containerId, globalServerName, globalDocumentRoot) {
+            var buildHierarchicalHost = function(name, vhost, containerId, globalServerName, globalDocumentRoot) {
                 
                 var documentRoot = (vhost.DocumentRoot == '' ? (globalDocumentRoot == '' ? 'unknown' : globalDocumentRoot)  : vhost.DocumentRoot);
                 var serverName = (vhost.ServerName == '' ? (globalServerName == '' ? 'unknown' : globalServerName)  : vhost.ServerName);
@@ -207,14 +294,14 @@ define([ "dojo/_base/declare",
                                     div.innerHTML = '<h5>Default</h5>';
                                     dom.byId('name_virtual_host_container').appendChild(div);
                                     
-                                    buildVirtualHost(host, hostArray[i], "name_virtual_host_container", globalServerName, globalDocumentRoot);
+                                    buildHierarchicalHost(host, hostArray[i], "name_virtual_host_container", globalServerName, globalDocumentRoot);
                                     
                                     div = document.createElement('div');
                                     div.innerHTML = '<h5>Other Virtual Hosts</h5>';
                                     dom.byId('name_virtual_host_container').appendChild(div);
                                 } else {
                                 
-                                    buildVirtualHost(host, hostArray[i], "name_virtual_host_container", globalServerName, globalDocumentRoot);
+                                    buildHierarchicalHost(host, hostArray[i], "name_virtual_host_container", globalServerName, globalDocumentRoot);
                                 }
                             }
                                                         
@@ -225,7 +312,7 @@ define([ "dojo/_base/declare",
                             div.innerHTML = '<h4>' + host + '</h4>';
                             dom.byId('other_virtual_host_container').appendChild(div);
                             
-                            buildVirtualHost(host, hostArray[0], "other_virtual_host_container", globalServerName, globalDocumentRoot);                            
+                            buildHierarchicalHost(host, hostArray[0], "other_virtual_host_container", globalServerName, globalDocumentRoot);                            
                         }
                         
                     }
@@ -239,6 +326,8 @@ define([ "dojo/_base/declare",
             );
             
         }
+        
+        //------------------END OF HIERARCHICAL VIRTUAL HOSTS-------------------------//
     
     });
     
