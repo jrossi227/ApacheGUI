484dc62f25dbc66e25ac2a5427baf23cb7dcf635
Jonathan Rossi <jonathan.rossi@shocase.com>
2015-12-26 21:53:07 -0500

    Refresh Global Tree when a configuration file changes

--- src/main/webapp/resources/net/apachegui/GlobalTree.js
+++ src/main/webapp/resources/net/apachegui/GlobalTree.js
@@ -11,10 +11,14 @@ define([
 
         initialized: false,
         configTree: null,
+        checkModifiedTimes: true,
+        lastModifiedTimes: [],
 
         init: function () {
             if(this.initialized===false) {
                 this.buildGlobalTree();
+                this.initLastModifiedTimes();
+                this.launchLastModifiedUpdateInterval();
                 this.initialized=true;
             }
         },
@@ -35,19 +39,109 @@ define([
         },
 
         buildGlobalTree: function() {
+            var that = this;
+
             this.configTree = new ConfigurationTree({
                 id: 'global_tree',
                 loadTreeJSON: this.loadGlobalTreeJSON
             });
             this.configTree.startup();
-            this.addListeners();
 
             domConstruct.place(this.configTree.domNode, dom.byId('global_tree_container'), 'last');
+
+            this.configTree.on("beforeAddLine", function(type, value) {
+                that.checkModifiedTimes = false;
+            });
+            this.configTree.on("afterAddLine", function(type, value) {
+               that.refreshModifiedTimes();
+            });
+            this.configTree.on("beforeEditLine", function(type, value) {
+                that.checkModifiedTimes = false;
+            });
+            this.configTree.on("afterEditLine", function(type, value) {
+                that.refreshModifiedTimes();
+            });
+            this.configTree.on("beforeDeleteLine", function(type, value) {
+                that.checkModifiedTimes = false;
+            });
+            this.configTree.on("afterDeleteLine", function(type, value) {
+                that.refreshModifiedTimes();
+            });
         },
 
-        addListeners: function() {
+        initLastModifiedTimes: function() {
             var that = this;
 
+            this.getLastModifiedTimes(function(lastModifiedTimes) {
+                that.lastModifiedTimes = lastModifiedTimes;
+            });
+        },
+
+        getLastModifiedTimes : function(callback) {
+            net.apachegui.Main.getInstance().getActiveFileList(function(files) {
+
+                net.apachegui.Main.getInstance().getLastModifiedTimes(
+                    files,
+                    function(response) {
+                        callback(response.lastModifiedTimes);
+                    }
+                );
+
+            });
+        },
+
+        refreshModifiedTimes: function() {
+            var that = this;
+            this.getLastModifiedTimes(function(lastModifiedTimes) {
+                that.checkModifiedTimes = true;
+                that.lastModifiedTimes = lastModifiedTimes;
+            })
+        },
+
+        checkIfModifiedTimesHaveChanged: function() {
+            var that = this;
+            if(this.lastModifiedTimes.length == 0) {
+                return;
+            }
+            this.getLastModifiedTimes(function(lastModifiedTimes) {
+
+                var reload = false;
+                if(that.lastModifiedTimes.length != lastModifiedTimes.length) {
+                    reload = true;
+                }
+
+                for(var i=0; i<that.lastModifiedTimes.length && !reload; i++) {
+                    var filtered = lastModifiedTimes.filter(function(lastModfiedTime) {
+                       if( that.lastModifiedTimes[i].file == lastModfiedTime.file &&
+                           that.lastModifiedTimes[i].lastModifiedTime == lastModfiedTime.lastModifiedTime) {
+                           return true;
+                       }
+
+                        return false;
+                    });
+
+                    if(filtered.length == 0) {
+                        reload = true;
+                    }
+
+                }
+
+                if(reload) {
+                    that.configTree.reload();
+                }
+
+                that.lastModifiedTimes = lastModifiedTimes;
+            });
+        },
+
+        launchLastModifiedUpdateInterval: function() {
+            var that = this;
+
+            net.apachegui.Interval.setInterval(function() {
+                if (that.checkModifiedTimes) {
+                    that.checkIfModifiedTimesHaveChanged();
+                }
+            }, 5000);
         }
 
     });
