6b5cece1b09896761251d773cb955229b2ee086c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-20 23:54:24 +0800

    Delete src/main directory

--- src/main/webapp/resources/net/apachegui/String.js
+++
@@ -1,7 +0,0 @@
-String.prototype.endsWith = function (str){
-    return this.slice(-str.length) == str;
-};
-
-String.prototype.startsWith = function (str){
-    return this.slice(0, str.length) == str;
-};
