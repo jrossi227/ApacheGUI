29201a28d0ca6f5c09a20591e0bb8510be3d8dba
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-20 23:55:28 +0800

    Delete src/main directory

--- src/main/java/net/apachegui/directives/BaseDirective.java
+++
@@ -1,9 +0,0 @@
-package net.apachegui.directives;
-
-public class BaseDirective {
-    protected String directiveName;
-
-    public BaseDirective(String directiveName) {
-        this.directiveName = directiveName;
-    }
-}
